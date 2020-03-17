ca7441d45d3939f2e6caa38fcb1bc4c9199483d3
Aditya Vishnu <adityavishnu3610@gmail.com>
2020-03-17 23:32:00 +0530

    Top Banner

--- index.php
+++ index.php
@@ -4,8 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Organic Basket | Home</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-    
+
+    <div class="" id="top"> <!-- begenning of top -->
+
+        <div class="container"> <!-- begenning of container -->
+
+            <div class="col-md-6 offer"> <!-- col-md-6 offer begin -->
+
+                <a href="#" class="btn btn-success btn-sm">Welcome</a>
+                <a href="checkout.php">4 Items In Your Cart | Total Price: $300</a>
+
+            </div> <!-- end of col-md-6 offer -->
+
+            <div class="col-md-6"> <!-- col-md-6 begin -->
+            
+                <ul class="menu"> <!-- menu begin -->
+                    <li>
+                        <a href="customer_register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">My Account</a>
+                    </li>
+                    <li>
+                        <a href="cart.php">Go to Cart</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">Login</a>
+                    </li>
+                </ul> <!-- menu finish -->
+
+            </div> <!-- col-md-6 end -->
+            
+        </div> <!-- end of container -->
+
+    </div> <!-- end of top -->
+
+<script src="js/jquery-331.min.js"></script>
+<script src="js/bootstrap-337.min.js"></script>
+
 </body>
 </html>
