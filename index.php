c02d7049162b8d8d247274e3926a61fc69a85248
Aditya Vishnu <adityavishnu3610@gmail.com>
2020-03-18 18:10:18 +0530

    Main Menu

--- index.php
+++ index.php
@@ -44,6 +44,94 @@
 
     </div> <!-- end of top -->
 
+    <div id="navbar" class="navbar navbar-default"> <!-- navbar start -->
+    
+        <div class="container"> <!-- container start -->
+
+            <div class="navbar-header"> <!-- navbar-header start -->
+
+                <a href="index.php" class="navbar-brand home"> <!-- navbar-brand home start -->
+                    <img src="images/ecom-store-logo.png" alt="Organic Basket Logo" class="hidden-xs">
+                    <img src="images/ecom-store-logo-mobile.png" alt="Organic Basket Logo" class="visible-xs">
+                </a> <!-- navbar-brand home finish -->
+
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <i class="fa fa-align-justify"></i>
+                </button>
+
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                    <span class="sr-only">Toggle Search</span>
+                    <i class="fa fa-search"></i>
+                </button>
+
+            </div> <!-- navbar-header finish -->
+
+            <div class="navbar-collapse collapse" id="navigation"> <!-- navbar-collapse collapse start -->
+
+                <div class="padding-nav"> <!-- padding-nav begin -->
+
+                    <ul class="nav navbar-nav left"> <!-- nav navbar-nav left begin -->
+                        <li class="active">
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="shop.php">Shop</a>
+                        </li>
+                        <li>
+                            <a href="checkout.php">My Account</a>
+                        </li>
+                        <li>
+                            <a href="cart.php">Shopping Cart</a>
+                        </li>
+                        <li>
+                            <a href="contact.php">Contact Us</a>
+                        </li>
+                    </ul> <!-- nav navbar-nav left finish -->
+
+                </div> <!-- padding-nav finish -->
+
+                <a href="cart.php" class="btn navbar-btn btn-primary right"> <!-- btn navbar-btn btn-primary right begin -->
+                
+                    <i class="fa fa-shopping-cart"></i>
+                    <span>4 Items in your cart</span>
+                
+                </a> <!-- btn navbar-btn btn-primary right finish -->
+
+                <div class="navbar-collapse collapse right"> <!-- navbar-collapse collapse right begin -->
+
+                    <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"> <!-- btn btn-primary navbar-btn begin -->
+
+                        <span class="sr-only">Toggle Search</span>
+                        <i class="fa fa-search"></i>
+
+                    </button> <!-- btn btn-primary navbar-btn finish -->
+
+                </div> <!-- navbar-collapse collapse right finish -->
+
+                <div class="collapse clearfix" id="search"> <!-- collapse clearfix begin --> <!-- SEARCH FROM -->
+                
+                    <form method="get" action="results.php" class="navbar-form"> <!-- navbar-form begin -->
+                    
+                        <div class="input-group"> <!-- input-group begin -->
+                            <input type="text" class="from-control" placeholder="Search" name="user_query" required>
+                            <span class="input-group-btn"> <!-- search button span -->
+                            <button type="submit" name="search" value="Search" class="btn btn-primary"> <!-- btn btn-primary begin -->
+                                <i class="fa fa-search"></i>
+                            </button> <!-- btn btn-primary finish -->
+                            </span> <!-- search button span -->
+                        </div> <!-- input-group finish -->
+                    
+                    </form> <!-- navbar-form finish -->
+                
+                </div> <!-- collapse clearfix finish --> <!-- SEARCH FROM ENDS -->
+
+            </div> <!-- navbar-collapse collapse finish -->
+
+        </div> <!-- container finish -->
+
+    </div> <!-- navbar finish -->
+
 <script src="js/jquery-331.min.js"></script>
 <script src="js/bootstrap-337.min.js"></script>
 
