02157c9ff90c8f662d61bfa76233b194ff3dbe55
Aditya Vishnu <adityavishnu3610@gmail.com>
2020-03-19 11:26:31 +0530

    Footer

---
+++ includes/footer.php
@@ -0,0 +1,115 @@
+<div id="footer"> <!-- footer begins -->
+    <div class="container"> <!-- container begins -->
+        <div class="row"> <!-- row begins -->
+            <div class="col-sm-6 col-md-3"> <!-- col-sm-6 col-md-3 begins -->
+
+                <h4>Pages</h4>
+
+                <ul> <!-- ul begins -->
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="checkout.php">My Account</a></li>
+                </ul> <!-- ul ends -->
+
+                <hr>
+
+                <h4>User Selection</h4>
+
+                <ul> <!-- ul begins -->
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul> <!-- ul ends -->
+
+                <hr class="hidden-md hidden-lg hidden-sm">
+
+            </div> <!-- col-sm-6 col-md-3 ends -->
+
+            <div class="col-sm-6 col-md-3"> <!-- col-sm-6 col-md-3 begins -->
+            
+                <h4>Top Categories</h4>
+
+                <ul> <!-- ul begins -->
+                    <li><a href="#">Grow Bags</a></li>
+                    <li><a href="#">Powdered Products</a></li>
+                    <li><a href="#">Packed Products</a></li>
+                    <li><a href="#">GoEarth</a></li>
+                    <li><a href="#">Flours</a></li>
+                </ul> <!-- ul ends -->
+
+                <hr class="hidden-md hidden-lg">
+            
+            </div> <!-- col-sm-6 col-md-3 ends -->
+
+            <div class="col-sm-6 col-md-3"> <!-- col-sm-6 col-md-3 begins -->
+            
+                <h4>Contact us</h4>
+
+                <p> <!-- p begins -->
+                
+                    <strong>Organic Basket Inc.</strong>
+                    <br>Kaloor
+                    <br>Ernakulam
+                    <br>Kerala, India
+                    <br>682017
+                    <br><a href="tel:+919947189437">+91 99471 89437</a>
+                    <br><a href="mailto:adityavishnu3610@gmail.com">adityavishnu3610@gmail.com</a>
+                
+                </p> <!-- p ends -->
+
+                <a href="contacts.php">Check our contact page</a>
+
+                <hr class="hidden-md hidden-lg">
+            
+            </div> <!-- col-sm-6 col-md-3 ends -->
+
+            <div class="col-sm-6 col-md-3">
+            
+                <h4>Get the news</h4>
+
+                <p class="text-muted">
+                
+                    Never miss an update from us. Keep in touch by subscribing to our mailing list.
+                
+                </p>
+
+                <form action="" method="post"> <!-- form begins -->
+                    <div class="input-group"> <!-- input-group begins -->
+                    
+                        <input type="email" placeholder="name@example.com" class="form-control" name="email">
+                        <span class="input-group-btn">
+                            <input type="submit" value="Subscribe" class="btn btn-default">
+                        </span>
+                    
+                    </div> <!-- input-group ends -->
+                </form> <!-- form ends -->
+
+                <hr>
+
+                <h4>Keep in touch</h4>
+
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                </p>
+            
+            </div>
+
+        </div> <!-- row ends -->
+    </div> <!-- container ends -->
+</div> <!-- footer ends -->
+
+<div id="copyright"> <!-- copyright begins -->
+    <div class="container"> <!-- container begins -->
+
+        <div class="col-md-6"> <!-- col-md-6 begins -->
+            <p class="pull-left">
+
+                &copy 2020 OrganicBasket. All rights reserved
+
+            </p>
+        </div> <!-- col-md-6 ends -->
+
+    </div> <!-- container ends -->
+</div> <!-- copyright ends -->
--- index.php
+++ index.php
@@ -435,6 +435,10 @@
         </div> <!-- row ends -->
     </div> <!-- container ends -->
 
+    <?php
+        include("includes/footer.php");
+    ?>
+
 <script src="js/jquery-331.min.js"></script>
 <script src="js/bootstrap-337.min.js"></script>
 
