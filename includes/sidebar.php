9df7548a549280bbac67376f25bc41636cd6a2b7
Aditya Vishnu <adityavishnu3610@gmail.com>
2020-03-19 15:53:07 +0530

    Shop.php

---
+++ includes/sidebar.php
@@ -0,0 +1,33 @@
+<div class="panel panel-default sidebar-menu"> <!-- panel panel-default sidebar-menu begins -->
+    <div class="panel-heading"> <!-- panel-heading begins -->
+        <h3 class="panel-title">Categories</h3>
+    </div> <!-- panel-heading begins -->
+
+    <div class="panel-body"> <!-- panel-body begins -->
+        <ul class="nav nav-pills nav-stacked category-menu"> <!-- nav nav-pills nav-stacked category-menu begins -->
+            <li><a href="#">Flour</a></li>
+            <li><a href="#">Packed Products</a></li>
+            <li><a href="#">Grow Bags</a></li>
+            <li><a href="#">Spices</a></li>
+            <li><a href="#">GoEarth</a></li>
+        </ul> <!-- nav nav-pills nav-stacked category-menu ends -->
+    </div> <!-- panel-body ends -->
+
+</div> <!-- panel panel-default sidebar-menu ends -->
+
+<div class="panel panel-default sidebar-menu"> <!-- panel panel-default sidebar-menu begins -->
+    <div class="panel-heading"> <!-- panel-heading begins -->
+        <h3 class="panel-title">Top Rated</h3>
+    </div> <!-- panel-heading begins -->
+
+    <div class="panel-body"> <!-- panel-body begins -->
+        <ul class="nav nav-pills nav-stacked category-menu"> <!-- nav nav-pills nav-stacked category-menu begins -->
+            <li><a href="#">Flour</a></li>
+            <li><a href="#">Packed Products</a></li>
+            <li><a href="#">Grow Bags</a></li>
+            <li><a href="#">Spices</a></li>
+            <li><a href="#">GoEarth</a></li>
+        </ul> <!-- nav nav-pills nav-stacked category-menu ends -->
+    </div> <!-- panel-body ends -->
+
+</div> <!-- panel panel-default sidebar-menu ends -->
