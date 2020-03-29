04c6a02b0b969816c8daf0c46caa39d290925bde
Aditya Vishnu <adityavishnu3610@gmail.com>
2020-03-29 21:08:59 +0530

    cart page dynamic

--- cart.php
+++ cart.php
@@ -25,7 +25,15 @@ include("includes/header.php");
                     <form action="cart.php" method="post" enctype="multipart/form-data"> <!-- form begins -->
 
                         <h1>Shopping Cart</h1>
-                        <p class="text-muted">You have 4 items in your cart</p>
+
+                        <?php
+                        $ip_add = getRealIpUser();
+                        $select_cart = "select * from cart where ip_add='$ip_add'";
+                        $run_cart = mysqli_query($con,$select_cart);
+                        $count = mysqli_num_rows($run_cart);
+                        ?>
+
+                        <p class="text-muted">You have <?php echo $count; ?> item(s) in your cart</p>
                         <div class="table-responsive"> <!-- table-responsive begins -->
                         
                             <table class="table"> <!-- table begins -->
@@ -36,7 +44,7 @@ include("includes/header.php");
                                         <th colspan="2">Product</th>
                                         <th>Quantity</th>
                                         <th>Unit Price</th>
-                                        <th>Size</th>
+                                        <th>Weight</th>
                                         <th colspan="1">Delete</th>
                                         <th colspan="2">Sub-total</th>
                                     </tr> <!-- tr ends -->
@@ -44,114 +52,51 @@ include("includes/header.php");
                                 </thead> <!-- thead ends -->
 
                                 <tbody> <!-- tbody begins -->
-                                
-                                    <tr> <!-- tr begins -->
-                                        <td>
-                                            <img class="img-responsive" src="admin_area/product_images/product-thumbnail4.jpg" alt="Product 4">
-                                        </td>
-                                        <td>
-                                            <a href="#">Organic Pulses</a>
-                                        </td>
-                                        <td>
-                                            2
-                                        </td>
-                                        <td>
-                                            Rs. 130
-                                        </td>
-                                        <td>
-                                            S
-                                        </td>
-                                        <td>
-                                            <input type="checkbox" name="remove[]">
-                                        </td>
-                                        <td>
-                                            Rs. 260
-                                        </td>
-                                    </tr> <!-- tr ends -->
-                                
-                                </tbody> <!-- tbody ends -->
 
-                                <tbody> <!-- tbody begins -->
-                                
-                                    <tr> <!-- tr begins -->
-                                        <td>
-                                            <img class="img-responsive" src="admin_area/product_images/product-thumbnail4.jpg" alt="Product 4">
-                                        </td>
-                                        <td>
-                                            <a href="#">Organic Pulses</a>
-                                        </td>
-                                        <td>
-                                            2
-                                        </td>
-                                        <td>
-                                            Rs. 130
-                                        </td>
-                                        <td>
-                                            S
-                                        </td>
-                                        <td>
-                                            <input type="checkbox" name="remove[]">
-                                        </td>
-                                        <td>
-                                            Rs. 260
-                                        </td>
-                                    </tr> <!-- tr ends -->
-                                
-                                </tbody> <!-- tbody ends -->
+                                    <?php
+                                        $total = 0;
+                                        while($row_cart = mysqli_fetch_array($run_cart)){
+                                            $pro_id = $row_cart['p_id'];
+                                            $pro_wt = $row_cart['weight'];
+                                            $pro_qty = $row_cart['qty'];
 
-                                <tbody> <!-- tbody begins -->
+                                            $get_products = "select * from products where product_id='$pro_id'";
+                                            $run_products = mysqli_query($con,$get_products);
+                                            while($row_products = mysqli_fetch_array($run_products)){
+                                                $product_title = $row_products['product_title'];
+                                                $product_img1 = $row_products['product_img1'];
+                                                $only_price = $row_products['product_price'];
+                                                $sub_total = $row_products['product_price']*$pro_qty;
+                                                $total += $sub_total;
+                                            
+                                        
+                                    ?>
                                 
                                     <tr> <!-- tr begins -->
                                         <td>
-                                            <img class="img-responsive" src="admin_area/product_images/product-thumbnail4.jpg" alt="Product 4">
+                                            <img class="img-responsive" src="admin_area/product_images/<?php echo $product_img1; ?>" alt="Product 4">
                                         </td>
                                         <td>
-                                            <a href="#">Organic Pulses</a>
+                                            <a href="details.php?pro_id=<?php echo $pro_id; ?>"><?php echo $product_title; ?></a>
                                         </td>
                                         <td>
-                                            2
+                                            <?php echo $pro_qty; ?>
                                         </td>
                                         <td>
-                                            Rs. 130
+                                            <?php echo $only_price; ?>
                                         </td>
                                         <td>
-                                            S
+                                            <?php echo $pro_wt; ?>
                                         </td>
                                         <td>
-                                            <input type="checkbox" name="remove[]">
+                                            <input type="checkbox" name="remove[]" value="<?php echo $pro_id; ?>">
                                         </td>
                                         <td>
-                                            Rs. 260
+                                            &#8377; <?php echo $sub_total; ?>
                                         </td>
                                     </tr> <!-- tr ends -->
-                                
-                                </tbody> <!-- tbody ends -->
 
-                                <tbody> <!-- tbody begins -->
-                                
-                                    <tr> <!-- tr begins -->
-                                        <td>
-                                            <img class="img-responsive" src="admin_area/product_images/product-thumbnail4.jpg" alt="Product 4">
-                                        </td>
-                                        <td>
-                                            <a href="#">Organic Pulses</a>
-                                        </td>
-                                        <td>
-                                            2
-                                        </td>
-                                        <td>
-                                            Rs. 130
-                                        </td>
-                                        <td>
-                                            S
-                                        </td>
-                                        <td>
-                                            <input type="checkbox" name="remove[]">
-                                        </td>
-                                        <td>
-                                            Rs. 260
-                                        </td>
-                                    </tr> <!-- tr ends -->
+                                    <? } } ?>
                                 
                                 </tbody> <!-- tbody ends -->
 
@@ -159,8 +104,8 @@ include("includes/header.php");
                                 
                                     <tr> <!-- tr begins -->
                                     
-                                        <th colspan="5">Total</th>
-                                        <th colspan="2">Rs. 1200</th>
+                                        <th colspan="6" style="text-align:right">Total:</th>
+                                        <th colspan="1">&#8377; <?php echo $total; ?></th>
                                     
                                     </tr> <!-- tr ends -->
 
@@ -201,6 +146,22 @@ include("includes/header.php");
                     </form> <!-- form ends -->
                 </div> <!-- box ends -->
 
+                <?php
+                    function update_cart(){
+                        global $con;
+                        if(isset($_POST['update'])){
+                            foreach($_POST['remove'] as $remove_id){
+                                $delete_product = "delete from cart where p_id='$remove_id'";
+                                $run_delete = mysqli_query($con,$delete_product);
+                                if($run_delete){
+                                    echo "<script>window.open('cart.php','_self')</script>";
+                                }
+                            }
+                        }
+                    }
+                    echo @$up_cart = update_cart();
+                ?>
+
                 <div id="row same-height-row"> <!-- row same-height-row begins -->
                     <div class="col-md-3 col-sm-6"> <!-- col-md-3 col-sm-6 begins -->
                         <div class="box same-height headline"> <!-- box same-height headline begins -->
@@ -208,41 +169,31 @@ include("includes/header.php");
                         </div> <!-- box same-height headline ends -->
                     </div> <!-- col-md-3 col-sm-6 ends -->
 
-                    <div class="col-md-3 col-sm-6 center-responsive"> <!-- col-md-3 col-sm-6 center-responsive begins -->
-                        <div class="product same-height"> <!-- product same-height begins -->
-                            <a href="details.php">
-                                <img class="img-responsive" src="admin_area/product_images/product-thumbnail4.jpg" alt="Product 4">
-                            </a>
-                            <div class="text"> <!-- text begins -->
-                                <h3><a href="details.php">Pulses</a></h3>
-                                <p class="price">Rs.130</p>
-                            </div> <!-- text ends -->
-                        </div> <!-- product same-height ends -->
-                    </div> <!-- col-md-3 col-sm-6 center-responsive ends -->
+                    <?php
+                    $get_products = "select * from products order by rand() LIMIT 0,3";
+                    $run_products = mysqli_query($con,$get_products);
+                    while($row_products = mysqli_fetch_array($run_products)){
+                        $pro_id = $row_products['product_id'];
+                        $pro_title = $row_products['product_title'];
+                        $pro_price = $row_products['product_price'];
+                        $pro_img1 = $row_products['product_img1'];
 
-                    <div class="col-md-3 col-sm-6 center-responsive"> <!-- col-md-3 col-sm-6 center-responsive begins -->
-                        <div class="product same-height"> <!-- product same-height begins -->
-                            <a href="details.php">
-                                <img class="img-responsive" src="admin_area/product_images/product-thumbnail.jpg" alt="Product 1">
-                            </a>
-                            <div class="text"> <!-- text begins -->
-                                <h3><a href="details.php">Pulses</a></h3>
-                                <p class="price">Rs.130</p>
-                            </div> <!-- text ends -->
-                        </div> <!-- product same-height ends -->
-                    </div> <!-- col-md-3 col-sm-6 center-responsive ends -->
+                        echo "
 
-                    <div class="col-md-3 col-sm-6 center-responsive"> <!-- col-md-3 col-sm-6 center-responsive begins -->
-                        <div class="product same-height"> <!-- product same-height begins -->
-                            <a href="details.php">
-                                <img class="img-responsive" src="admin_area/product_images/product-thumbnail2.jpg" alt="Product 2">
-                            </a>
-                            <div class="text"> <!-- text begins -->
-                                <h3><a href="details.php">Pulses</a></h3>
-                                <p class="price">Rs.130</p>
-                            </div> <!-- text ends -->
-                        </div> <!-- product same-height ends -->
-                    </div> <!-- col-md-3 col-sm-6 center-responsive ends -->
+                        <div class='col-md-3 col-sm-6 center-responsive'> <!-- col-md-3 col-sm-6 center-responsive begins -->
+                            <div class='product same-height'> <!-- product same-height begins -->
+                                <a href='details.php?pro_id=$pro_id'>
+                                    <img class='img-responsive' src='admin_area/product_images/$pro_img1' alt='Product 4'>
+                                </a>
+                                <div class='text'> <!-- text begins -->
+                                    <h3><a href='details.php?pro_id=$pro_id'>$pro_title</a></h3>
+                                    <p class='price'>&#8377; $pro_price</p>
+                                </div> <!-- text ends -->
+                            </div> <!-- product same-height ends -->
+                        </div> <!-- col-md-3 col-sm-6 center-responsive ends -->
+                        ";
+                    }
+                    ?>
 
                 </div> <!-- row same-height-row ends -->
 
@@ -265,7 +216,7 @@ include("includes/header.php");
 
                                 <tr> <!-- tr begins -->
                                     <td>Order Sub-total</td>
-                                    <th>Rs. 1120</th>
+                                    <th>&#8377; <?php echo $total; ?></th>
                                 </tr> <!-- tr ends -->
 
                                 <tr> <!-- tr begins -->
@@ -280,7 +231,7 @@ include("includes/header.php");
 
                                 <tr class="total"> <!-- tr begins -->
                                     <td>Total</td>
-                                    <th>Rs. 1200</th>
+                                    <th>&#8377;<?php echo $total; ?></th>
                                 </tr> <!-- tr ends -->
                                 
                             </tbody> <!-- tbody ends -->
